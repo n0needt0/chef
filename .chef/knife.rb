40a812b5ecd155287654503453317e465d25a3fe
Andrew Yasinsky <andrew@yasinsky.com>
2014-09-09 12:28:57 -0700

    cleanup

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'andrew'
+client_key               '/home/andrewy/chef-repo/.chef/andrew.pem'
+validation_client_name   'chef-validator'
+validation_key           '/home/andrewy/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://chef.helppain.net'
+syntax_check_cache_path  '/home/andrewy/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/home/andrewy/chef-repo/cookbooks' ]
